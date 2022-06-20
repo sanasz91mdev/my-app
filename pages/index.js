e972198f0bf2be6a2f36da617f7a559b09c6ab0e
Sana Zehra <Sana.Zehra@tpsonline.com>
2022-06-20 11:49:47 +0500

    added server side props

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,11 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { Post } from '../src/models'
 
-export default function Home() {
+export default function Home({data}) {
+  console.log(data)
   const [posts, setPosts] = useState([])
 
+ 
   useEffect(() => {
     fetchPosts()
     async function fetchPosts() {
@@ -25,10 +27,21 @@ export default function Home() {
           <Link href={`/posts/${post.id}`}>
             <a>
               <h2>{post.title}</h2>
+              <h3>Data from API: {data.name}</h3>
             </a>
           </Link>
         ))
       }
     </div>
   )
+}
+
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://d2exmp6j4rjt6r.cloudfront.net/api/hello`)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
